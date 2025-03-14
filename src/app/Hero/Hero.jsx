1c8b67e8ee56cbb705d551f5ae5fb9472d8d24e0
Ayegba John <ayegbajohn@gmail.com>
2025-03-14 21:11:54 +0100

    Hero

---
+++ src/app/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className={styles.warpper}>
+        <div className={styles.main}></div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
