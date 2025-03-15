d23221f2efd55e91452e882d68f5e4f9ee1833f2
Ayegba John <ayegbajohn@gmail.com>
2025-03-15 23:32:35 +0100

    minor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Payment from "./components/Payment/Payment";
+import Hero from "./Hero";
 
 const page = () => {
   return (
     <div>
       <Payment />
+      <Hero />
     </div>
   );
 };
